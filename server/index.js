514db195beb375b80ff61e6704ebcefbd86a1140
AdrienSldy <a.sasia.ldy@cogiway.com>
2017-06-11 01:10:27 +0200

    Rooms
    Add multiple rooms functionnal

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ const socketIo = require('socket.io');
 const http = require('http');
 const server = http.Server(app);
 const io = socketIo(server);
-// websocket
-
-//io class Server
-//io.of(namespace) pour se connecter sur un namespace class Namespace
 
 
 const getQuestion = require('./question/QuestionController.js');
@@ -20,59 +16,77 @@ require('dotenv').config()
 server.listen(3100, function () {
   console.log('Example app listening on port 3100!');
 });
-/*
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-*/
 
 app.get(['/','/question','/question/:category'], getQuestion);
 
-const rooms = {
-  smash_lab: [],
-  lock: false
-}
-io.on('connection', (socket) => {
-  if(rooms.lock || rooms.smash_lab.length > 1)socket.disconnect(true);
-  else{
-    const index = 0 
-    socket.room = 'lab_' + index; 
-    rooms.smash_lab.push(socket.id);
-    console.log(rooms.smash_lab);
+app.use('/api', Api);
+
+function matching(socket){
+  if(!rooms.length){
+    socket.room = 'lab_0';
+    rooms.push({name: socket.room, players: [socket.id], lock: false});
     socket.join(socket.room);
-    socket.emit('lab', socket.id);
-
-    socket.on('start', () => {
-      socket.to(socket.room).broadcast.emit('start', socket.id);
+  }else{
+    let match = rooms.some((room, index) => {
+      if(room.players.length < 2 && !room.lock){
+        room.players.push(socket.id);
+        room.lock = true;
+        socket.room = room.name;
+        socket.join(socket.room);
+        return true;
+      }
     });
-
-    socket.on('match', () => {
-      rooms.lock = true;
-      socket.to(socket.room).broadcast.emit('match', socket.id);
-    })
-
-    socket.on('heroSelected', (hero_id) => {
-      socket.to(socket.room).broadcast.emit('enemySelection', hero_id);
-    });
-
-    socket.on('attack', (enemy) => {
-      socket.to(socket.room).broadcast.emit('attack', enemy);
-    });
+    if(!match){
+      socket.room = 'lab_' + (parseInt(rooms[rooms.length - 1].name.split("_")[1]) + 1);
+      rooms.push({name: socket.room, players: [socket.id], lock: false});
+      socket.join(socket.room);
+    }
   }
+}
+
+function clean(socket){
+  rooms.forEach((room, index) => {
+    if(room.players.includes(socket.id)){
+      console.log('user : ' + socket.id + ' delete of : ' + rooms[index].name);
+      room.players.splice(room.players.indexOf(socket.id), 1);
+      if(!room.players.length){
+         console.log('rooms delete : ' + rooms[index].name);
+        rooms.splice(index, 1);
+      }
+      console.log(rooms);
+    }
+  });
+}
+
+const rooms = [];
+
+io.on('connection', (socket) => {
+
+  matching(socket);
+
+  console.log('Room: ' + socket.room);
+  console.log('Rooms : ' + rooms);
+
+  socket.emit('lab', socket.id);
+
+  socket.on('start', () => {
+    socket.to(socket.room).broadcast.emit('start', socket.id);
+  });
+
+  socket.on('match', () => {
+    socket.to(socket.room).broadcast.emit('match', socket.id);
+  })
+
+  socket.on('heroSelected', (hero_id) => {
+    socket.to(socket.room).broadcast.emit('enemySelection', hero_id);
+  });
+
+  socket.on('attack', (enemy) => {
+    socket.to(socket.room).broadcast.emit('attack', enemy);
+  });
+
   socket.on('disconnect', () => {
-    rooms.smash_lab.splice(rooms.smash_lab.indexOf(socket.id), 1);
-    rooms.lock = rooms.smash_lab.length;
-    socket.to(socket.room).emit('friendDisconnect');
-    console.log(rooms.lock);
-    console.log(rooms.smash_lab);
-    // do some stuff
+    clean(socket);
     console.log('disconnect');
   });
 });
-
-
-app.use('/api', Api);
-
-
-
-
